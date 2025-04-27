4c1e923d41b0f3a375232537490ae95cd9b030fc
Irvin Ignacio Cueto Lopez <iirvin.cueto.02@gmail.com>
2025-04-27 15:58:42 -0600

    feat: add user relationship to Comentario model and display comments with usernames in post view

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -52,8 +52,24 @@
                     </form>
                 @endauth
 
-                <div>
-                    {{ dd($post->comentarios) }}
+                <div class="bg-white shadow mb-5 max-h-96 overflow-y-scroll mt-5">
+                    @if ($post->comentarios->count())
+                        @foreach ($post->comentarios as $comentario)
+                            <div class="p-5 border-gray-300 border-b">
+                                <a href="{{ route('posts.index', $comentario->user) }}" class="font-bold">
+                                    {{ $comentario->user->username }}
+                                </a>
+                                <p>
+                                    {{ $comentario->comentario }}
+                                </p>
+                                <p class="text-sm text-gray-500">
+                                    {{ $comentario->created_at->diffForHumans() }}
+                                </p>
+                            </div>
+                        @endforeach
+                    @else
+                        <p class="p-10 text-center">No Hay comentarios Aún</p>
+                    @endif
                 </div>
 
             </div>
